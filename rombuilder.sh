a9ad9027dbb27796c1d963eb159e2103f2713d71
Ako-Ina <65094216+RebuiltStatue21@users.noreply.github.com>
2021-11-20 22:18:07 -0700

    added questions

---
+++ rombuilder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo
+echo Welcome to the Rom Builder.
+echo
+echo Please fimilar yourself with these Android terms:
+echo
+echo ...
+read -p "Press Enter/Return to continue, or [CTRL+C] to exit script: "
+echo
+echo
+echo How much ccash do you want (HIGHLY RECOMMENDED! Recommended ccash size is ~25-50GB per device):
+echo
+echo What Rom project would you like to build? (Please provide repo link. This is found on github (projectname/Android)):
+echo
+echo What is your device codename?:
+echo
+echo Please provide your devices proprietary blobs:
+echo
+echo Please select build type (vanilla/gapps):
+echo
+echo Would you like to sign your ROM (Y/N)?:
+echo
+echo Installing required packages for the android build enviornment.
+echo Installing Android/device repos.
+echo
+echo How many processes do you want to run (the more the faster it will build):
+echo
+echo Starting build:
+sleep 3
+echo
+echo Rom zip is located at (~/projectname/out/target/product/devicecodename)
+echo
+echo Have fun testing out the rom you just created!
+echo I hope this script has been helpful.
