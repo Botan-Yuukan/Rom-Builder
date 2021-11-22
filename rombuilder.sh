bf5ee762426c74374e91a74d3ecdf99ea1afbf18
Ako-Ina <65094216+Ako-Ina36@users.noreply.github.com>
2021-11-22 14:54:04 -0700

    fixed bugs/added read commands

--- rombuilder.sh
+++ rombuilder.sh
@@ -21,37 +21,34 @@ read -p "Press Enter/Return to continue, or [CTRL+C] to exit script: "
 echo
 echo Please fimilar yourself with these basic commands:
 echo
-echo ls <dir>
-echo cd <dir>
-echo rm -rf <dir/file>
-echo mkdir <dir>
+echo "ls <dir>"
+echo "cd <dir>"
+echo "rm -rf <dir/file>"
+echo "mkdir <dir>"
 echo
 echo Also fimilar yourself with these basic github commands:
 echo
-echo git clone <repo_url> <path_to_go> -b <branch>
-echo git push <remote> <branch>
-echo git chechkout -b <branch>
-echo git commit
-echo git cherry-pick <commit_id>
-echo git reverse <commit_id>
+echo "git clone <repo_url> <path_to_go> -b <branch>"
+echo "git push <remote> <branch>"
+echo "git chechkout -b <branch>"
+echo "git commit"
+echo "git cherry-pick <commit_id>"
+echo "git reverse <commit_id>"
 echo
-echo read -p "Press Enter/Return to continue, or [CTRL+C] to exit script: "
-echo
-echo
-echo
+read -p "Press Enter/Return to continue, or [CTRL+C] to exit script: "
 echo
 echo You will need to have the folling to continue:
 echo
 echo 1. Know your device codename name
 echo 2. Have your devices proprietary blobs
-echo    1. You can decide if you want to: extract them from your phone
-echo    2. If your phone is already offically supports LineageOS find your proprietary blobs from https://github.com/TheMuppets
+echo .. 1. You can decide if you want to: extract them from your phone
+echo .. 2. If your phone is already offically supports LineageOS find your proprietary blobs from https://github.com/TheMuppets
 echo 3. Know what project you are building for
 echo 4. Know your project build flags
 echo 5. What build type do you want to build
-echo    1. vanilla
-echo    2. gapps
-echo    3. custom if available
+echo .. 1. vanilla
+echo .. 2. gapps
+echo .. 3. custom if available
 echo
 read -p "Press Enter/Return to continue, or [CTRL+C] to exit script: "
 # aditional info
@@ -67,28 +64,46 @@ read -p "Press Enter/Return to continue, or [CTRL+C] to exit script: "
 echo
 echo User Setup:
 echo
-echo How much ccash do you want? (HIGHLY RECOMMENDED! Recommended ccash size is ~25-50GB per device):
+echo "How much ccash do you want? (HIGHLY RECOMMENDED! Recommended ccash size is ~25-50GB per device): "
 echo
-echo What Rom project would you like to build? (Please provide repo link. This is found on github (projectname/Android)):
+read ccash
 echo
-echo What is your device codename?:
+echo "What Rom project would you like to build? (Please provide repo link. This is found on github (projectname/Android)): "
 echo
-echo Please provide your devices proprietary blobs:
+read rom
 echo
-echo Please select build type (usally vanilla/gapps):
+echo "What is your device codename?: "
 echo
-echo Would you like to sign your ROM (Y/N)?:
+read codename
+echo
+echo "Please provide your devices proprietary blobs: "
+echo
+read devicepropietaryblobs
+echo
+echo "Please select build type (usally vanilla/gapps):"
+echo
+read buildtype
+echo
+echo "What would you like to put as the maintainer name?: "
+echo
+read maintainer name
+echo
+echo "Would you like to sign your ROM (Y/N)?: "
+echo
+read signrom
 echo
 echo Installing required packages for the android build enviornment.
 echo Installing Android/device repos.
 echo
-echo How many processes do you want to run (the more the faster it will build):
+echo "How many processes do you want to run (the more the faster it will build): "
 echo
-echo Starting build:
+read processcount
+echo
+echo "Starting build for ${codename}: "
 sleep 3
 # add install script here
 echo
-echo Rom zip is located at ~/projectname/out/target/product/devicecodename/romname-ocean.zip
+echo "Rom zip is located at ~/projectname/out/target/product/devicecodename/romname-ocean.zip"
 echo
 # End Info
 echo Ending Info:
